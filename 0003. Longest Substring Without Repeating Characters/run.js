ee786efc817d182a392750d29adbddaab67c6c59
tsupox <tsupox@amazon.co.jp>
2022-10-14 14:22:01 +0900

    3  dvdf, ' ' はいけたけど cdd がいけない

--- 0003. Longest Substring Without Repeating Characters/run.js
+++ 0003. Longest Substring Without Repeating Characters/run.js
@@ -4,36 +4,40 @@
  * @see https://leetcode.com/problems/longest-substring-without-repeating-characters/
  */
 var lengthOfLongestSubstring = function (s) {
-  if (s.length == 0) return 0
+  // if (s.length == 0) return 0
 
-  let longest = s[0]
-  let current = s[0]
+  let longest = ""
+  let current = ""
 
-  for (let i = 1; i < s.length; i++) {
-    // 現在の文字列に入ってなければ
-    if (!(new RegExp(s[i])).test(current)) {
-      current += s[i]
-
-      // 最後の時は else に入らないので
-      if (i == s.length - 1) {
-        longest = longest.length < current.length ? current : longest
+  for (let i = 0; i < s.length; i++) {
+    let regResult = (new RegExp(s[i])).exec(s.slice(i + 1))
+    current = s.slice(i, (regResult ? (i + regResult.index + 1) : s.length))
+    let isAllOnce = true
+    for (let j = 0; j < current.length; j++) {
+      if ((new RegExp(current[j])).exec(current.slice(j + 1))) {
+        isAllOnce = false
+        break;
       }
-    } else {
-      longest = longest.length < current.length ? current : longest
-      current = s[i]
     }
-  }
+    if (isAllOnce) {
+      longest = longest.length < current.length ? current : longest
 
+    }
+
+  }
   return longest.length
 };
+
 //-----
 
 let inputs = [
-  "abcabcbb", //3
-  "bbbbb",  //1
-  "pwwkew", //3
-  "au", //2
-  "dvdf", //3
+  // "abcabcbb", //3
+  // "bbbbb",  //1
+  // "pwwkew", //3
+  // "au", //2
+  // "dvdf", //3
+  // " ", //1
+  "cdd", //2
 ]
 
 for (const i in inputs) {
